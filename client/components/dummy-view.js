19bfd9f285ec44a32f58fd2b7429e9cccee9c3e9
Iuliia <54540652+YuLogun@users.noreply.github.com>
2019-10-25 18:16:17 +0400

    hooks

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -1,15 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
 import Head from './head'
 
 const Dummy = () => {
-  const [counter] = useState(0)
+  const [counter, setCounter] = useState(0)
+  const [date, setDate] = useState(new Date())
+  useEffect(
+    () => {
+      const intervalId = setInterval(
+        () => { setCounter(counter + 1) },
+        1000
+      )
+      return () => { clearInterval(intervalId) }
+    },
+    [counter]
+  )
+  useEffect(
+    () => {
+      const intervalId = setInterval(
+        () => { setDate(new Date()) },
+        1000
+      )
+      return () => { clearInterval(intervalId) }
+    }
+  )
   return (
     <div>
       <Head title="Hello" />
-      <div> Hello World {counter} </div>
+      <div className="red"> Hello World! We&apos;re counting {counter}
+        <p>
+          <button
+            type="button"
+            onClick={() => setCounter(counter + 1)}
+          >Add
+          </button>
+        </p>
+        <p>
+          <button
+            type="button"
+            onClick={() => setCounter(counter - 1)}
+          >Subtract
+          </button>
+        </p>
+        <p>
+          <button
+            type="button"
+            onClick={() => setCounter(0)}
+          >Reset
+          </button>
+        </p>
+      </div>
+      <div className="date">
+        <h3>It is {date.toLocaleTimeString()}</h3>
+      </div>
     </div>
   )
 }
